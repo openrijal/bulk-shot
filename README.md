6150495fa98bb1ab4ac4cce0c15da84cc629b1de
Nitesh Rijal <rijal.it@gmail.com>
2016-06-07 13:48:34 +0545

    basic screenshot done for slimer and phantom

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # bulk-shot
  a simple utility to get PhantomJS and SlimerJS screenshots providing a list of urls in a file
+
+# installation
+
+## install npm dependencies
+
+`npm install`
+
+# running the package
+
+## run phantom screenshot
+
+`phantomjs phanshot.js`
+
+## run slimer screenshot
+
+`slimerjs slimshot.js`
+
+# running slimerjs in headless mode
+
+## install Xvfb
+
+### Debian Based
+
+`apt-get install xvfb`
+
+### RPM Based
+
+`yum install xorg-X11-server-Xvfb`
+
+## run slimer in Xvfb
+
+`xvfb-run slimerjs slimshot.js`
---
+++ slimshots.js
@@ -0,0 +1,86 @@
+// Render Multiple URLs to file
+
+"use strict";
+var RenderUrlsToFile, arrayOfUrls, system;
+
+system = require("system");
+var fs = require('fs');
+var file_buffer = fs.open('urls.txt', 'r');
+
+var all_urls = file_buffer.read().split("\n");
+
+
+function cleanArray(actual) {
+  var newArray = new Array();
+  for (var i = 0; i < actual.length; i++) {
+    if (actual[i]) {
+      newArray.push(actual[i]);
+    }
+  }
+  return newArray;
+}
+
+var urls = cleanArray(all_urls);
+
+file_buffer.close();
+
+/*
+Render given urls
+@param array of URLs to render
+@param callbackPerUrl Function called after finishing each URL, including the last URL
+@param callbackFinal Function called after finishing everything
+*/
+RenderUrlsToFile = function(urls, callbackPerUrl, callbackFinal) {
+    var getFilename, next, page, retrieve, urlIndex, webpage;
+    urlIndex = 0;
+    webpage = require("webpage");
+    page = null;
+    getFilename = function() {
+        return 'slimer_output/' + urlIndex + ".jpg";
+    };
+    next = function(status, url, file) {
+        page.close();
+        callbackPerUrl(status, url, file);
+        return retrieve();
+    };
+    retrieve = function() {
+        var url;
+        if (urls.length > 0) {
+            url = urls.shift();
+            urlIndex++;
+            page = webpage.create();
+            page.viewportSize = {
+                width: 800,
+                height: 600
+            };
+            page.settings.userAgent = "Slimer.js bot";
+            return page.open(url, function(status) {
+                var file;
+                file = getFilename();
+                if (status === "success") {
+                    return window.setTimeout((function() {
+                        page.render(file);
+                        return next(status, url, file);
+                    }), 10000);
+                } else {
+                    return next(status, url, file);
+                }
+            });
+        } else {
+            return callbackFinal();
+        }
+    };
+    return retrieve();
+};
+
+arrayOfUrls = null;
+
+RenderUrlsToFile(urls, (function(status, url, file) {
+    if (status !== "success") {
+        return console.log("Unable to render '" + url + "'");
+    } else {
+        return console.log("Rendered '" + url + "' at '" + file + "'");
+    }
+}), function() {
+    return phantom.exit();
+});
